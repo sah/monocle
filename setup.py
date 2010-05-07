2abb733fbf579c1970370efd4e7c2aea8fb4c922
sah <sah@awesame.org>
2010-05-07 02:13:17 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="monocle",
+    version="0.1",
+    description="An async programming framework with a blocking look-alike syntax",
+    author="Greg Hazel and Steven Hazel",
+    author_email="sah@awesame.org",
+    maintainer="Steven Hazel",
+    maintainer_email="sah@awesame.org",
+    py_modules=['monocle']
+)
