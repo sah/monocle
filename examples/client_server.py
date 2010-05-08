247ad20e008e73734624562c7cc69f3726763501
sah <sah@awesame.org>
2010-05-08 01:06:20 -0700

    minor readability improvement

--- examples/client_server.py
+++ examples/client_server.py
@@ -2,8 +2,8 @@ import sys
 import time
 
 import monocle
-monocle.init(sys.argv[1])
 from monocle import _o
+monocle.init(sys.argv[1])
 
 from monocle.stack import eventloop
 from monocle.stack.network import add_service, Service, Client
--- examples/echo_server.py
+++ examples/echo_server.py
@@ -1,8 +1,8 @@
 import sys
 
 import monocle
-monocle.init(sys.argv[1])
 from monocle import _o
+monocle.init(sys.argv[1])
 
 from monocle.stack import eventloop
 from monocle.stack.network import add_service, Service
