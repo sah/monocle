7bf78e151e736aec077e6694e2479e8875eb5812
Steven Hazel <sah@saucelabs.com>
2010-10-07 14:31:38 -0700

    a better abbreviation for this variable

--- monocle/util.py
+++ monocle/util.py
@@ -2,6 +2,6 @@ from monocle.stack.eventloop import queue_task
 from monocle.callback import Callback
 
 def sleep(seconds):
-    d = Callback()
-    queue_task(seconds, d.trigger, None)
-    return d
+    cb = Callback()
+    queue_task(seconds, cb.trigger, None)
+    return cb
