a65b993a3b8cfb49dbee45b9bda4318660bb37ec
Greg Hazel <ghazel@gmail.com>
2011-07-15 16:12:13 -0700

    fix indentation to 4 spaces

--- examples/tcp_proxy.py
+++ examples/tcp_proxy.py
@@ -12,23 +12,23 @@ from monocle.stack.network import add_service, Service, Client, ConnectionLost
 
 @_o
 def pump(input, output):
-  while True:
-    try:
-      message = yield input.read_some()
-      yield output.write(message)
-    except ConnectionLost:
-      output.close()
-      break
-    except socket.error:
-      output.close()
-      break
+    while True:
+        try:
+            message = yield input.read_some()
+            yield output.write(message)
+        except ConnectionLost:
+            output.close()
+            break
+        except socket.error:
+            output.close()
+            break
 
 @_o
 def handle_socks(conn):
-  client = Client()
-  yield client.connect('localhost', 8050)
-  monocle.launch(pump, conn, client)
-  yield pump(client, conn)
+    client = Client()
+    yield client.connect('localhost', 8050)
+    monocle.launch(pump, conn, client)
+    yield pump(client, conn)
 
 add_service(Service(handle_socks, port=7050))
 eventloop.run()
