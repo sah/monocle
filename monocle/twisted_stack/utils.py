da56986a9aba0f18357c4b35dc0de7135e621074
Steven Hazel <sah@saucelabs.com>
2011-08-26 19:06:09 -0700

    a useful utility when working directly with Twisted

---
+++ monocle/twisted_stack/utils.py
@@ -0,0 +1,11 @@
+from twisted.python.failure import Failure
+from twisted.internet.defer import Deferred
+
+def cb_to_df(cb):
+    df = Deferred()
+    def call_deferred_back(v, df=df):
+        if isinstance(v, Exception):
+            df.errback(Failure(v, type(v), None))
+        else:
+            df.callback(v)
+    cb.add(call_deferred_back)
