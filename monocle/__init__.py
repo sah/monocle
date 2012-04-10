fd540e0d4ed0b6e6ac6907df49d94607c85be6a3
Steven Hazel <sah@saucelabs.com>
2012-04-10 15:39:25 -0700

    version 0.18

--- monocle/__init__.py
+++ monocle/__init__.py
@@ -3,7 +3,7 @@ import sys
 import core
 from core import _o, o, launch, Return, InvalidYieldException, log_exception
 
-VERSION = '0.17'
+VERSION = '0.18'
 
 _stack_name = None
 def init(stack_name):
