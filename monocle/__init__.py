08262b2474e6ee0e587e6444b6389002b726f987
Steven Hazel <sah@saucelabs.com>
2012-11-02 17:49:35 -0700

    version 0.22

--- monocle/__init__.py
+++ monocle/__init__.py
@@ -3,7 +3,7 @@ import sys
 import core
 from core import _o, o, launch, Return, InvalidYieldException, log_exception
 
-VERSION = '0.21'
+VERSION = '0.22'
 
 _stack_name = None
 def init(stack_name):
